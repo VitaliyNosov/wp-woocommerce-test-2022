d71a8692e2576090259ef7da3621dfa8b19c26de
VitaliyNosov <webdevelorestudio2017@gmail.com>
2022-02-16 03:06:44 +0200

    work

--- functions.php
+++ functions.php
@@ -217,4 +217,94 @@ function ace_quantity_input_field_args( $args, $product ) {
 }
 add_filter( 'woocommerce_quantity_input_args', 'ace_quantity_input_field_args', 10, 2 );
 
+// Кастомная форма поиска
+
+function true_search_form( $form ) {
+    $form = '	
+		<form role="search" method="get" class="search-form" action="http://woocommrce-test/">
+			<label>
+				<span class="screen-reader-text">Search for:</span>
+				<input type="search" class="search-field" placeholder="Search …" value="" name="s">
+			</label>
+			<input type="submit" class="search-submit" value="">
+		</form>	
+	'; // в эту переменную записываем новую поисковую форму
+    return $form;
+}
+ 
+add_filter( 'get_search_form', 'true_search_form' );
+
+
+
+// woocommerce custom icons
+
+
+/*
+ * Shortcode for WooCommerce Cart Icon for Menu Item
+ */
+add_shortcode ('woocommerce_cart_icon', 'woo_cart_icon' );
+function woo_cart_icon() {
+    ob_start();
+ 
+        $cart_count = WC()->cart->cart_contents_count; // Set variable for cart item count
+        $cart_url = wc_get_cart_url();  // Set variable for Cart URL
+  
+        echo '<li><a class="menu-item cart-contents" href="'.$cart_url.'" title="Cart">';
+        
+        if ( $cart_count > 0 ) {
+        
+            echo '<span class="cart-contents-count">'.$cart_count.'</span>';
+       
+        }
+        
+        echo '</a></li>';
+        
+            
+    return ob_get_clean();
+ 
+}
+
+
+/*
+ * Filter with AJAX When Cart Contents Update
+ */
+
+add_filter( 'woocommerce_add_to_cart_fragments', 'woo_cart_icon_count' );
+function woo_cart_icon_count( $fragments ) {
+ 
+    ob_start();
+    
+    $cart_count = WC()->cart->cart_contents_count;
+    $cart_url = wc_get_cart_url();
+    
+    
+    echo '<a class="cart-contents menu-item" href="'.$cart_url.'" title="View Cart">';
+    
+    if ( $cart_count > 0 ) {
+        
+        echo '<span class="cart-contents-count">'.$cart_count.'</span>';
+                    
+    }
+    echo '</a>';
+ 
+    $fragments['a.cart-contents'] = ob_get_clean();
+     
+    return $fragments;
+}
+
+
+/*
+ * Append Cart Icon Particular Menu
+ */
+add_filter('wp_nav_menu_items','woo_cart_icon_menu', 10, 2);
+function woo_cart_icon_menu($menu, $args) {
+
+    if($args->theme_location == 'primary') { // 'primary' is my menu ID
+        $cart = do_shortcode("[woo_cart_but]");
+        return $cart . $menu;
+    }
+
+    return $menu;
+}
+
 
--- header.php
+++ header.php
@@ -10,6 +10,7 @@
  */
 
 ?>
+
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
@@ -20,40 +21,65 @@
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'test-woocommerce' ); ?></a>
+<div class="header-top-block">
+	<span>FREE U.S. SHIPPING 90-DAY RETURNS</span>
+</div>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$test_woocommerce_description = get_bloginfo( 'description', 'display' );
-			if ( $test_woocommerce_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $test_woocommerce_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+	<div class="container">
+	<header>
+		<div class="logo-block">
+			<span>COMPANY NAME</span>
+		</div>
+		<div class="category-block">
+			<span>WOMEN</span>
+			<span>|</span>
+			<span>MEN</span>
+		</div>
+		<div class="sherch-block">
+			<?php get_search_form(); ?>
+		</div>
+		<div class="button-block">
+			<div class="button-icon">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/Favorites.png" alt="#">
+			</div>
+			<div class="button-icon">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/user.png" alt="#">
+			</div>
+			<div class="button-icon">
+				<?php echo do_shortcode( '[woocommerce_cart_icon]' );?>
+			</div>		
+		</div>
+	</header>
+		<div class="tag-block">
+			<div class="tag sale">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/1.png" alt="#">
+			</div>
+			<div class="tag new">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/7.png" alt="#">
+			</div>
+			<div class="tag bras">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/4.png" alt="#">
+			</div>
+			<div class="tag swimsuits">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/2.png" alt="#">
+			</div>
+			<div class="tag sleepwear">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/8.png" alt="#">	
+			</div>
+			<div class="tag underwear">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/5.png" alt="#">		
+			</div>
+			<div class="tag sexy-lingerine">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/3.png" alt="#">
+			</div>
+			<div class="tag womens-branding">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/6.png" alt="#">
+			</div>
+					
+			
+			
+		</div>
+	</div>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'test-woocommerce' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+
+	
